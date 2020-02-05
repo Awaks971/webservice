1e1e5e42caf7f5702ee6e047d278b0cae356b8b7
Nicolas <nicolas.leroy971@gmail.com>
2020-02-05 09:39:38 -0400

    Review auth verification middleware

--- middlewares/auth_verification.js
+++ middlewares/auth_verification.js
@@ -1,14 +1,27 @@
 const DB = require("../config/database");
 
 async function user_verification_middleware(req, res, next) {
-  const { uuid_Personne: userId } = req.body;
+  const { uuid_magasin: companyId } = req.body;
+  const [potential_company] = await DB.queryAsync(
+    `SELECT id, status, owner FROM company WHERE id="${companyId}"`
+  );
+  if (!potential_company || !potential_company.id) {
+    res
+      .status(401)
+      .json({ message: "Unknown company", error_code: "COMPANY_NOT_FOUND" });
+    res.end();
+    return;
+  }
+
   const [potential_user] = await DB.queryAsync(
-    `SELECT id, status FROM users WHERE id="${userId}"`
+    `SELECT id, status FROM users WHERE id="${potential_company.owner}"`
   );
   if (!potential_user || !potential_user.id) {
     res
       .status(401)
       .json({ message: "Unknown user", error_code: "USER_NOT_FOUND" });
+    res.end();
+    return;
   }
   if (potential_user.status === "WAITING") {
     res.status(403).json({
@@ -16,6 +29,8 @@ async function user_verification_middleware(req, res, next) {
         "This user is waiting for an admin validation, please contact awaks support",
       error_code: "USER_WAITING_FOR_VALIDATION"
     });
+    res.end();
+    return;
   }
   if (potential_user.status === "LOCKED") {
     res.status(403).json({
@@ -23,6 +38,8 @@ async function user_verification_middleware(req, res, next) {
         "This user account is is locked by awaks, please contact awaks support",
       error_code: "LOCKED_USER"
     });
+    res.end();
+    return;
   }
 
   next();
