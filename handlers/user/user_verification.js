c79a313376dd03a03b74f99ab199af43f260c1d5
Nicolas <nicolas.leroy971@gmail.com>
2020-02-10 14:29:25 -0400

    some stuff

--- handlers/user/user_verification.js
+++ handlers/user/user_verification.js
@@ -1,44 +1,38 @@
 const DB = require("../../config/database");
 
 const user_status = {
-  VALIDATED: "VALIDATED",
-  WAITING: "WAITING",
-  LOCKED: "LOCKED"
+  VALIDATED: "validated",
+  WAITING: "waiting",
+  LOCKED: "locked"
 };
 
-async function user_verification(req, res, next) {
-  const { uuid_Personne: userId } = req.body;
+async function user_verification(req, res) {
+  const { company_id } = req.body;
   const [potential_user] = await DB.queryAsync(
-    `SELECT id, status FROM users WHERE id="${userId}"`
+    `SELECT id, status FROM user WHERE company_id="${company_id}"`
   );
 
   if (!potential_user || !potential_user.id) {
-    res
+    return res
       .status(401)
       .json({ message: "Unknown user", error_code: "USER_NOT_FOUND" });
-    res.end();
-    return;
   }
   if (potential_user.status === user_status.WAITING) {
-    res.status(403).json({
+    return res.status(403).json({
       message:
         "This user is waiting for an admin validation, please contact awaks support",
       error_code: "USER_WAITING_FOR_VALIDATION"
     });
-    res.end();
-    return;
   }
   if (potential_user.status === user_status.LOCKED) {
-    res.status(403).json({
+    return res.status(403).json({
       message:
         "This user account is is locked by awaks, please contact awaks support",
       error_code: "LOCKED_USER"
     });
-    res.end();
-    return;
   }
 
-  res.status(200).json({
+  return res.status(200).json({
     message: "Known user, feel free to store your data :)",
     error_code: null
   });
